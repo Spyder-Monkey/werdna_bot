36d168aee8733230c832298207d3458ded40981f
Trevor Bender <trevor.bender99@gmail.com>
2021-12-30 10:46:12 -0500

    updated secrets env file

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ import os
 from dotenv import load_dotenv
 
 
-client = discord.Client
-
-# print(client.get_user)
-
-
 # Overridden method
 # Event method for when message is sent into chat
 # Message parameter holds all information about message
@@ -18,23 +13,15 @@ async def on_message(self, message):
     if message.author == client.user: return
     if str(message.author) == 'ArborO#7508':
         # get topic of message and webscrapte wikipedia    
+        pass
 
+# Load secrets file and get token
+load_dotenv('.env')
+API_TOKEN = os.getenv('DISCORD_TOKEN')
 
-#URL = 'https://en.wikipedia.org/wiki/Suburb'
-#page = requests.get(URL)
+client = discord.Client
 
-#soup = BeautifulSoup(page.content, 'html.parser')
+client.run(API_TOKEN)
 
-#results = soup.find(id="bodyContent")
-#print(results.prettify())
-
-#section = results.find_all('div', class_='card-content')
-
-#for job_element in job_elements:
-#    title_element = job_element.find('h2', class_='title')
-#    company_element = job_element.find('h3', class_='company')
-#    location_element = job_element.find('p', class_='location')
-#    print(title_element.text.strip())
-#    print(company_element.text.strip())
-#    print(location_element.text.strip())
-#    print()
+for member in client.get_all_members():
+    print(member)
