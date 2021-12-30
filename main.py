2be551871784d167d8f45ca2d1b0d06e2be27990
Trevor Bender <trevor.bender99@gmail.com>
2021-12-30 11:13:36 -0500

    connects to discord server

--- main.py
+++ main.py
@@ -5,22 +5,27 @@ import os
 from dotenv import load_dotenv
 
 
-# Overridden method
-# Event method for when message is sent into chat
-# Message parameter holds all information about message
-async def on_message(self, message):
-    # Safety so bot doesn't inifinitely respond to himself
-    if message.author == client.user: return
-    if str(message.author) == 'ArborO#7508':
-        # get topic of message and webscrapte wikipedia    
-        pass
+class WerdnaBot(discord.Client):
+    def __init__(self):
+        super().__init__()
 
-# SECRET FILE TESTS
+    # Overridden method
+    # Event method for when message is sent into chat
+    # Message parameter holds all information about message
+    async def on_message(self, message):
+        # Safety so bot doesn't inifinitely respond to himself
+        if message.author == client.user: return
+        if str(message.author) == 'ArborO#7508':
+            # get topic of message and webscrapte wikipedia    
+            pass
+        if str(message.author) == 'Spyder#5038':
+            print("SPYDER")
+
+
 
 # Load secrets file and get token
-load_dotenv('.env')
+load_dotenv('secrets.env')
 API_TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client
-
+client = WerdnaBot()
 client.run(API_TOKEN)
